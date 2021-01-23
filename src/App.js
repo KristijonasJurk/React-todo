3abb05d28df08d0875d27490f830d01349dcd7ed
KristijonasJurk <70882614+KristijonasJurk@users.noreply.github.com>
2021-01-23 21:22:27 +0200

    APP.JS APP.CSS

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React, { useState } from 'react';
-import Tweet from "./Tweet";
+// import React, { useState } from 'react';
+// import Tweet from "./Tweet";
+
 // import components
+import Nav from "./components/Nav";
+import Tweets from "./components/Tweets";
+
 
 function App() {
 
   return (
-    <div className='main'>
-
+    <div className="app">
+      <div className="home">
+        <Nav />
+        <Tweets />
+      </div>
     </div>
   )
 }
---
+++ src/components/Tweets.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Tweets = () => (
+    <section>
+        <div>hi</div>
+    </section>
+);
+
+export default Tweets;
