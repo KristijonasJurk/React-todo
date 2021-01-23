38e758f0955ac2230f67cb566cd1d348f12dc8d2
KristijonasJurk <70882614+KristijonasJurk@users.noreply.github.com>
2021-01-23 22:38:11 +0200

    react counter

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
-import React from "react";
+// import React from "react";
 import "./App.css";
-// import React, { useState } from 'react';
+import React, { useState } from 'react';
 
 // import components
-import Nav from "./components/Nav";
-import Tweets from "./components/Tweets";
+// import Nav from "./components/Nav";
+// import Tweets from "./components/Tweets";
 
 
 function App() {
 
+  const [counter, setCounter] = useState(0)
+
+  const incrementer = () => {
+    setCounter((prev) => prev + 1);
+    console.log(counter);
+  }
+
   return (
     <div className="app">
       <div className="home">
-        <Nav />
-        <Tweets />
+        <h2>counter {counter}</h2>
+        <button onClick={incrementer}>click</button>
+        {/* <Nav />
+        <Tweets /> */}
       </div>
     </div>
   );
