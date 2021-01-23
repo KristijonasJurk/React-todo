f3b4bbf52c5eeb8650cef9dec2a033eb84c547da
KristijonasJurk <70882614+KristijonasJurk@users.noreply.github.com>
2021-01-23 21:07:41 +0200

    added nav.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import React, { useState } from 'react';
 import Tweet from "./Tweet";
-
+// import components
 
 function App() {
 
-  const [isBlue, setBlue] = useState(false);
-  const blue = () => {
-    setBlue(!isBlue);
-  }
-  const [users, setUsers] = useState([
-    { name: 'Tommy', message: 'hey whats up' },
-    { name: 'Jacky', message: 'la la la' }
-  ])
-
   return (
     <div className='main'>
-      {users.map(user => (
-        <Tweet title={user.name} message={user.message} />
-      ))}
-      <h1 className={isBlue ? "blue" : ""}>Change color</h1>
-      <button onClick={blue}>change</button>
+
     </div>
   )
 }
--- src/Tweet.js
+++ src/Tweet.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import "./App.css";
+// import React from 'react';
+// import "./App.css";
 
-function Tweet({ title, message }) {
-    return (
+// function Tweet({ title, message }) {
+//     return (
 
-        <div className='box'>
-            <h1>{title}</h1>
-            <p>{message}</p>
-            <h3>some other text</h3>
-        </div>
-    )
-}
+//         <div className='box'>
+//             <h1>{title}</h1>
+//             <p>{message}</p>
+//             <h3>some other text</h3>
+//         </div>
+//     )
+// }
 
-export default Tweet;
+// export default Tweet;
