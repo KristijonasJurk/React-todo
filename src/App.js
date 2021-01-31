30ed1185b8193df5f29175d2bead72c5fa83e7eb
KristijonasJurk <70882614+KristijonasJurk@users.noreply.github.com>
2021-01-31 20:55:37 +0200

    todo list

--- src/App.js
+++ src/App.js
@@ -2,36 +2,14 @@
 import "./App.css";
 import React, { useState } from 'react';
 
-// import components
-import Nav from "./components/Nav";
-import Video from "./components/Video";
-
 
 function App() {
 
-  const [counter, setCounter] = useState(0)
-  const [toggle, setToggle] = useState(false);
 
-  const incrementer = () => {
-    setCounter(counter + 1);
-    console.log(counter);
-  }
-  const toggler = () => {
-    setToggle(prev => !prev);
-  }
 
   return (
     <div className="app">
-      <div className="home">
-        <h1 className={toggle ? 'active' : ""}>nematomas</h1>
-        <h2>counter {counter}</h2>
-        <button onClick={incrementer}>click</button>
-        <button onClick={toggler}>toggle</button>
-        <Nav toggle={toggle} />
-        <Video nr={counter} setToggle={toggler} />
-        {/* <Nav />
-        <Tweets /> */}
-      </div>
+
     </div>
   );
 }
