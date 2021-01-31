ae91c0248417c31c08654db96b93433a214b3fc9
KristijonasJurk <70882614+KristijonasJurk@users.noreply.github.com>
2021-01-31 21:10:04 +0200

    single todo

--- src/App.js
+++ src/App.js
@@ -3,13 +3,35 @@ import "./App.css";
 import React, { useState } from 'react';
 
 
-function App() {
 
+
+function App() {
+  const [todos, setTodos] = useState(
+    {
+      text: 'hellooooo',
+      isComplete: false
+    },
+    {
+      text: 'byeeeeeee',
+      isComplete: false
+    },
+    {
+      text: 'hiiiiiiiii',
+      isComplete: false
+    }
+  )
 
 
   return (
     <div className="app">
-
+      {todos.map((todo, index) =>
+        <div className="todo-list">
+          <Todo
+            index={index}
+            todo={todo}
+          ></Todo>
+        </div>
+      )}
     </div>
   );
 }
